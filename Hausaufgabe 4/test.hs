f8862cb248e7f8e24c8eb7d83840c0a027eebab4
conchulio <waltersheridan@gmail.com>
2013-05-16 15:56:36 +0200

    Added other function

--- Hausaufgabe 4/test.hs
+++ Hausaufgabe 4/test.hs
@@ -1,6 +1,16 @@
-p n = 1/(fromIntegral n)
+p :: Int -> Double
+p n = 1/((fromIntegral n) + 1)
 
+p' :: Int -> Double
 p' n | n <= 1    = p 1
      | otherwise = (1 - (p' $ n-1)) * (p n)
 
-main = putStrLn $ show $ p' 20
+prev :: Int -> Double
+prev n =
+        let
+          nPlus = [1,2..(n-1)]
+          mapped = map p' nPlus
+        in
+          foldl (+) 0 mapped
+
+main = putStrLn $ show $ prev 50
