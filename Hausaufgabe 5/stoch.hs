d1291dd933af22956df6fdd6596e0336b826a62a
conchulio <waltersheridan@gmail.com>
2013-05-21 01:26:56 +0200

    Finished Stochastik

--- Hausaufgabe 5/stoch.hs
+++ Hausaufgabe 5/stoch.hs
@@ -1,21 +1,37 @@
--- P(X = n)
+-- base function
 p :: Int -> Double
 p n = 1/((fromIntegral n) + 1)
 
+-- P(X = n)
+pEquals :: Int -> Double
+pEquals n =
+  let
+    prev = [1..n-1]
+    unsuccessful = if length prev > 0
+      then
+        foldl1 (*) $ map (\n -> 1-(p n)) [1..n-1]
+      else
+        1
+    alltogether = unsuccessful * (p n)
+  in
+    alltogether
+
+-- P(X <= n)
+pLessThanOrEquals :: Int -> Double
+pLessThanOrEquals n = foldl1 (+) $ map pEquals [1..n]
+
 -- P(X > n)
-p' :: Int -> Double
-p' n =
-        let
-          range = [1..n]
-          mapped = map p range
-        in
-          foldl1 (*) mapped
+pGreaterThan :: Int -> Double
+pGreaterThan n = (-) 1 $ pLessThanOrEquals n
+
+list n = map p [1..n]
 
 main = do
-  let n = 2
-  putStr $ "p(" ++ (show n) ++ ") = "
-  putStrLn $ show $ p n
-  putStr $ "p'(" ++ (show n) ++ ") = "
-  putStrLn $ show $ p' n
+  let n = 20
+  --putStr $ "pLessThanOrEquals(" ++ (show n) ++ ") = "
+  --putStrLn $ show $ pLessThanOrEquals n
+  --putStr $ "pEquals(" ++ (show n) ++ ") = "
+  --putStrLn $ show $ pEquals n
+  putStrLn $ show $ list n
 
-  -- the results seem legit
+
